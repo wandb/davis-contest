cc26cdb85a8bb03863aa6466963571fdb1acdbfc
Charles Frye <charles@wandb.com>
2021-01-27 18:16:58 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name="contest",
+      version="0.1",
+      description="Tools for Weights & Biases DAVIS Video Segmentation Contest",
+      url="https://github.com/wandb/davis-contest/"
+      )
