811c86556db522421848d52a5172556154c94c74
Charles Frye <charles@wandb.com>
2021-01-27 18:39:00 -0800

    Create utils.py

---
+++ contest/utils.py
@@ -0,0 +1,49 @@
+import pandas as pd
+
+
+def fetch_paths(artifact, column=None):
+  """From an artifact, get the paths of associated files.
+  As a side effect, downloads the Artifact to local file storage
+  if it is not present.
+
+  Parameters:
+    artifact: wandb.Artifact
+    column: None or string
+      If string, name of column of paths to fetch from Artifact's paths_df
+      If None, return the entire dataframe of paths
+
+  Returns:
+    paths: pd.DataFrame or pd.Series
+      Paths to files attached to Artifact
+  """
+  directory = artifact.download()
+  paths = rebase_paths(get_paths(artifact, column), directory)
+  return  paths
+
+
+def rebase_paths(paths, rebase_dir):
+  if isinstance(paths, pd.Series):
+    method = paths.map
+  elif isinstance(paths, pd.DataFrame):
+    method = paths.applymap
+
+  return method(lambda s: os.path.join(rebase_dir, s))
+
+
+def get_paths(artifact, column=None):
+  try:
+    paths_filename = artifact.get_path("paths.json").download()
+  except KeyError:
+    raise Exception("Artifact did not contain a path.json file at top-level.\n"
+                    "All dataset and result Artifacts need a paths.json file listing associated files.")
+
+  paths_df = pd.read_json(paths_filename)
+  if column is not None:
+    try:
+      paths_series = paths_df[column]
+    except KeyError:
+      raise KeyError(f"could not find column {column} in {paths_filename}")
+  else:
+    return paths_df
+
+  return paths_series
