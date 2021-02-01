c9fb4b74f9da883876dec82592df3f9dc5c2a1e9
Charles Frye <charles@wandb.com>
2021-02-01 01:10:08 -0800

    add data class for keras utils

---
+++ contest/keras_utils/data.py
@@ -0,0 +1,33 @@
+import math
+
+import numpy as np
+import skimage.io
+
+class VidSegDatasetSequence(keras.utils.Sequence):
+
+  def __init__(self, image_paths, annotation_paths=None, batch_size=32):
+    self.image_paths, self.annotation_paths = image_paths, annotation_paths
+    self.batch_size = batch_size
+
+  def __len__(self):
+    return math.ceil(len(self.image_paths) / self.batch_size)
+
+  def __getitem__(self, idx):
+    image_paths = self.image_paths.iloc[self._batch_start(idx): self._batch_start(idx + 1)]
+    if self.annotation_paths is not None:
+      annotation_paths = self.annotation_paths.iloc[self._batch_start(idx): self._batch_start(idx + 1)]
+    else:
+      annotation_paths = None
+
+    images = np.array([skimage.io.imread(path) for path in image_paths])
+    
+    if annotation_paths is not None:
+      annotations = [skimage.io.imread(path) for path in annotation_paths]
+      annotations = np.array(annotations) / 255.
+      return images, annotations
+
+    else:
+      return images
+
+  def _batch_start(self, idx):
+    return idx * self.batch_size
