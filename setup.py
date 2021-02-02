962429b5635c8f49ed73502dbe679c47f9a85822
Charles Frye <charles@wandb.com>
2021-02-02 15:20:09 -0800

    updates setup.py with conditional installs

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 from setuptools import find_packages, setup
 
 setup(name="contest",
-      version="0.1",
+      version="0.2",
       description="Tools for Weights & Biases DAVIS Video Segmentation Contest",
       url="https://github.com/wandb/davis-contest/",
-      packages=find_packages()
+      packages=find_packages(),
+      python_requires=">=3.6.9",
+      install_requires=[
+            "numpy",
+            "pandas",
+            "Pillow",
+            "wandb"
+      ],
+      extras_require={
+            "keras": ["scikit-image", "tensorflow"],
+            "torch": ["ptflops", "pytorch_lightning", "scikit-image", "torchvision"]
+      }
       )
