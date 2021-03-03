f43eaed98fd9d95fb897a3d80fb488bf5ecc922a
Charles Frye <charles@wandb.com>
2021-03-02 16:59:59 -0800

    Handles artifact name fetching for wandb>0.10.20

--- contest/torch_utils/utils.py
+++ contest/torch_utils/utils.py
@@ -16,6 +16,11 @@ def save_model_to_artifact(model, path, name, artifact_path="final_model"):
   torch.save(model.state_dict(), path)
   model_artifact.add_file(path, artifact_path)
   wandb.run.log_artifact(model_artifact)
+  
+  try:
+    model_artifact.wait()
+  except AttributeError:
+    pass
 
   return "/".join([wandb.run.entity, wandb.run.project, model_artifact.name])
 
